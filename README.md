2c72597985e4dbc48df206a50da8026b288e215d
xryuseix <51394682+xryuseix@users.noreply.github.com>
2024-06-18 05:27:44 +0900

    feat: support multiprocess bin

--- README.md
+++ README.md
@@ -2,9 +2,22 @@
 
 ## Usage
 
+some binary
+
 ```bash
-deno task run sleep 1000                                                                                                                                                                                        ─╯
+$ deno task run sleep 1000
 Task run deno run --allow-run --allow-sys main.ts "sleep" "1000"
 current memory usage: 416
 max memory usage: 976
+```
+
+Chrome
+
+```bash
+$ deno task run --multi=chrome /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --remote-debugging-port=9222 --user-data-dir=../chrome-user-data
+stderr: 
+DevTools listening on ws://127.0.0.1:9222/devtools/browser/c04b7315-6835-4133-b8f7-92f7d109bb89
+
+current memory usage: 4122064^C
+max memory usage: 4122064 KB
 ```
--- main.ts
+++ main.ts
@@ -3,18 +3,26 @@ let process: Deno.ChildProcess;
 
 function setSignals() {
   Deno.addSignalListener("SIGINT", () => {
-    console.log(`max memory usage: ${maxMemory}`);
+    console.log(`\nmax memory usage: ${maxMemory} KB`);
     Deno.exit(1);
   });
 }
 
-async function getMemUsage() {
+async function getMemUsage(multiProcess: string | undefined) {
   if (!process) {
     console.error("Process is not running");
     return 0;
   }
+
+  const args = multiProcess
+    ? [
+      "-c",
+      `ps aux | grep -i ${multiProcess} | tail -n +2 | awk -F ' ' '{sum += $6} END {print sum}'`,
+    ]
+    : ["-c", `ps -p ${process.pid} -o rss | grep -v RSS`];
+
   const watcher = new Deno.Command("/bin/sh", {
-    args: [ "-c",`ps -p ${process.pid} -o rss | grep -v RSS`],
+    args,
     stdout: "piped",
     stderr: "piped",
   });
@@ -30,10 +38,17 @@ async function getMemUsage() {
 }
 
 function main() {
-  const cmd = Deno.args[0];
+  let multiProcess: string | undefined = undefined;
+  const args = Deno.args.flatMap((arg) => {
+    if (arg.startsWith("--multi")) {
+      multiProcess = arg.split("=")?.[1] ?? undefined;
+      return [];
+    }
+    return [arg];
+  });
   setSignals();
 
-  const p = new Deno.Command(cmd, {
+  const p = new Deno.Command(args[0], {
     args: Deno.args.slice(1),
     stdout: "piped",
     stderr: "piped",
@@ -41,12 +56,24 @@ function main() {
   process = p.spawn();
 
   const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+
+  process.stdout.getReader().read().then((v) => {
+    const stdout = decoder.decode(v.value);
+    console.log(`\nstdout: ${stdout}`);
+  });
+  process.stderr.getReader().read().then((v) => {
+    const stderr = decoder.decode(v.value);
+    console.log(`\nstderr: ${stderr}`);
+  });
 
   setInterval(async () => {
-    const memUsage = await getMemUsage();
+    const memUsage = await getMemUsage(multiProcess);
     maxMemory = Math.max(maxMemory, memUsage);
-    Deno.stdout.write(encoder.encode(`\r\x1b[0Kcurrent memory usage: ${memUsage}`));
-  }, 500)
+    Deno.stdout.write(
+      encoder.encode(`\r\x1b[0Kcurrent memory usage: ${memUsage}`),
+    );
+  }, 500);
 }
 
 main();
